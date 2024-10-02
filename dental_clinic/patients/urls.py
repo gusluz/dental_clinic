84a11bf7472d5e531476f55d64e23e8f3877b0c5
gusluz <henriqueglfs@hotmail.com>
2024-10-02 15:59:35 -0400

    CRUD Patients 100% completed

--- dental_clinic/patients/urls.py
+++ dental_clinic/patients/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import PatientsListView, PatientsCreateView, PatientsDetailView
+from .views import *
 
 
 app_name = 'patients'
 urlpatterns = [
     path('list', PatientsListView.as_view(), name='list'),
     path('add', PatientsCreateView.as_view(), name='add'),
-    path('info/<pk>', PatientsDetailView.as_view(), name='info'),
+    path('info/<int:pk>', PatientsDetailView.as_view(), name='info'),
+    path('edit/<int:pk>', PatientsUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>', PatientsDeleteView.as_view(), name='delete'),
 ]
--- dental_clinic/patients/views.py
+++ dental_clinic/patients/views.py
@@ -1,22 +1,43 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views import generic
 from .models import Patients
 from .forms import PatientForm
 from django.urls import reverse_lazy
+from django.http import JsonResponse
 
 
-class PatientsListView(ListView):
+class PatientsListView(generic.ListView):
     template_name = 'patients/patients_list.html'
     model = Patients
     context_object_name = 'patients'
 
 
-class PatientsCreateView(CreateView):
+class PatientsCreateView(generic.CreateView):
     template_name = 'patients/patients_add.html'
     model = Patients
     form_class = PatientForm
     success_url = reverse_lazy('patients:list')
 
 
-class PatientsDetailView(DetailView):
+class PatientsDetailView(generic.DetailView):
     template_name = 'patients/patients_info.html'
     model = Patients
+    context_object_name = 'patient'
+
+
+class PatientsUpdateView(generic.UpdateView):
+    template_name = 'patients/patients_edit.html'
+    model = Patients
+    fields = '__all__'
+    context_object_name = 'patient'
+    success_url = reverse_lazy('patients:list')
+
+
+class PatientsDeleteView(generic.DeleteView):
+    model = Patients
+    success_url = reverse_lazy('patients:list')
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        if request.is_ajax():
+            return JsonResponse({'redirect_url': self.success_url})
+        return response
